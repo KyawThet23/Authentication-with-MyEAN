6bd1b9837f798988eae88488c013df2ae35d5583
kyawThetHtun <kyawthe235@gmail.com>
2024-03-23 10:42:55 +0630

    register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  }
+  
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() : void 
+  {
+    const { username, email, password } = this.form;
+
+    this.authService.register(username,email,password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    })
+  }
 }
