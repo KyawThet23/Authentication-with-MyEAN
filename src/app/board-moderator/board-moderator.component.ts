e9774df162ef5c2687d9f7ca09a47950d0264d19
kyawThetHtun <kyawthe235@gmail.com>
2024-03-29 15:05:12 +0630

    role based pages

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_service/user.service';
 
 @Component({
   selector: 'app-board-moderator',
   templateUrl: './board-moderator.component.html',
   styleUrl: './board-moderator.component.css'
 })
-export class BoardModeratorComponent {
+export class BoardModeratorComponent implements OnInit {
+
+  content?: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getModeratorBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {console.log(err)
+        if (err.error) {
+          this.content = JSON.parse(err.error).message;
+        } else {
+          this.content = "Error with status: " + err.status;
+        }
+      }
+    });
+  }
 
 }
--- src/app/board-use/board-use.component.ts
+++ src/app/board-use/board-use.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_service/user.service';
 
 @Component({
   selector: 'app-board-use',
   templateUrl: './board-use.component.html',
   styleUrl: './board-use.component.css'
 })
-export class BoardUseComponent {
+export class BoardUseComponent implements OnInit{
 
+  content?: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUserBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {console.log(err)
+        if (err.error) {
+          this.content = JSON.parse(err.error).message;
+        } else {
+          this.content = "Error with status: " + err.status;
+        }
+      }
+    });
+  }
 }
